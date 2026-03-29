4c8915e83ba62dc61cee26a8527d8803c72b4073
minhiwu_286 <hieuacong555@gmail.com>
2026-03-29 15:14:13 +0700

    Code file theme for UIHelper.cs

---
+++ SecureChat.Client/Helpers/UIHelper.cs
@@ -0,0 +1,13 @@
+using System.Drawing;
+
+namespace SecureChat.Client.Helpers
+{
+    public static class Theme
+    {
+        public static Color BgMain = Color.FromArgb(33, 33, 33);
+        public static Color BgSecondary = Color.FromArgb(44, 44, 44);
+        public static Color Accent = Color.FromArgb(0, 136, 204);
+        public static Color Text = Color.White;
+        public static Color TextSecondary = Color.Gray;
+    }
+}
